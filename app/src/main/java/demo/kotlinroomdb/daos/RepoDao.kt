6181cdedb473ff4afa1d61eb00016066ecbd54c8
abhijeetvader9287 <abhijeetvader9287@gmail.com>
2018-08-02 15:32:51 +0530

    Revert "create entities"

---
+++ app/src/main/java/demo/kotlinroomdb/daos/RepoDao.kt
@@ -0,0 +1,20 @@
+package demo.kotlinroomdb.daos
+
+
+import androidx.room.*
+import demo.kotlinroomdb.entities.Repo
+
+@Dao
+interface RepoDao {
+    @get:Query("SELECT * FROM repo")
+    val allRepos: List<Repo>
+
+    @Insert
+    fun insert(vararg repos: Repo)
+
+    @Update
+    fun update(vararg repos: Repo)
+
+    @Delete
+    fun delete(vararg repos: Repo)
+}
