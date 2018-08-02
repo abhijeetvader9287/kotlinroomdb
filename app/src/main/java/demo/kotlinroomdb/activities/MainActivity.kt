5739ad31feeb513c6aeeb9eaf210e84666d054d2
Abhijeet Vader <a.vader@innopix.solutions>
2018-08-02 13:07:40 +0530

    first commit

---
+++ app/src/main/java/demo/kotlinroomdb/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package demo.kotlinroomdb.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import demo.kotlinroomdb.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
